6f6ac1637bb3f918af9c3dfdd215f3865124426d
Ajay Krishna Ayyappan <47910395+Ajay10422@users.noreply.github.com>
2024-04-25 15:56:56 -0400

    Add files via upload

---
+++ Crop.py
@@ -0,0 +1,90 @@
+import streamlit as st
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from langchain_community.document_loaders import CSVLoader
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_openai.embeddings import OpenAIEmbeddings
+
+def initialize_chain():
+    csv_loader = CSVLoader(r"AgroQA Dataset1.csv")
+    docs = csv_loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter()
+    documents = text_splitter.split_documents(docs)
+
+    embeddings = OpenAIEmbeddings(openai_api_key="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")
+
+    vectorstore = FAISS.from_documents(documents, embeddings)
+
+    retriever = vectorstore.as_retriever()
+
+    template = """Answer the question based only on the following context:
+    {context}
+    Give as much description as possible.
+
+    Question: {question}
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+
+    model = ChatOpenAI(openai_api_key="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")
+
+    output_parser = StrOutputParser()
+
+    setup_and_retrieval = RunnableParallel(
+        {"context": retriever, "question": RunnablePassthrough()}
+    )
+    chain = setup_and_retrieval | prompt | model | output_parser
+    
+    return chain
+
+chain = initialize_chain()
+
+
+def main():
+
+    from openai import OpenAI
+    import streamlit as st
+
+    st.title("AgroQA Chatbot")
+
+    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("What is up?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        if ("maize" or "beans" or "casava" or 'general') in prompt:
+                with st.chat_message("assistant"):
+                        response = chain.invoke(prompt)
+                        st.write(response)
+        else:
+
+            with st.chat_message("assistant"):
+                stream = client.chat.completions.create(
+                    model=st.session_state["openai_model"],
+                    messages=[
+                        {"role": m["role"], "content": m["content"]}
+                        for m in st.session_state.messages
+                    ],
+                    stream=True,
+                )
+                response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+if __name__ == "__main__":
+    main()
